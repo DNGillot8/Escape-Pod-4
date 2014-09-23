e007551acef5385a8a93886fa141c7eca884ee71
wesley-t <toneywg1@gcc.edu>
2014-09-23 19:26:45 -0400

    Update kingsCorners.cpp
    Added Column class, all include and using statements, and the srand(time(0)) in main

--- Main Program/kingsCorners/kingsCorners/kingsCorners.cpp
+++ Main Program/kingsCorners/kingsCorners/kingsCorners.cpp
@@ -1,11 +1,90 @@
 // kingsCorners.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include <iostream>
+#include "Card.h"
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::ios;
+using std::string;
+using std::vector;
+using std::random_shuffle;
+using std::rand;
+using std::srand;
 
 
-int _tmain(int argc, _TCHAR* argv[])
+
+class Column {
+public:
+	//void checkIfValid(); //make sure alternating colors, might not be needed
+	Column(vector<Card> c);
+	Column();
+	void addCard(Card c);
+	void removeCard(Card c);
+	Card getTop() const; //returns the top card of the column
+	Card getBottom() const; //returns the bottom card of the column
+	Card getCard(int i) const; //returns the card at index i
+	void shuffle();
+	int size() const;
+	void clear();
+private:
+	vector<Card> cards;
+};
+
+Column::Column(vector<Card> c) {
+	cards = c;
+}
+
+Column::Column() {
+	vector<Card> c;
+	cards = c;
+}
+
+void Column::addCard(Card c) {
+	cards.push_back(c);
+}
+
+//maybe don't need
+void Column::removeCard(Card c) {
+	cards.pop_back();
+}
+
+//probably don't need
+Card Column::getCard(int i) const {
+	return cards.at(i);
+}
+
+Card Column::getTop() const {
+	return cards.at(cards.size() - 1);
+}
+
+Card Column::getBottom() const {
+	return cards.at(0);
+}
+
+void Column::clear() {
+	cards.clear;
+}
+
+void Column::shuffle() {
+	random_shuffle(cards.begin(), cards.end());
+}
+
+int Column::size() const {
+	return cards.size();
+}
+
+
+int main()
 {
+	srand(time(0))
 	return 0;
 }
 
